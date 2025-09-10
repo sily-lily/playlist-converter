4761d3d73d8f70b2b12ad399c972dd6d34378599
sily-lily <ilovesuno@proton.me>
2025-09-10 14:57:49 -0800

    Coloring class inspired by Roblox's Color3

---
+++ build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const RGB_1 = require("./types/RGB");
+console.log(RGB_1.Color3.fromRGB(0, 200, 0).toAnsi("hello world"), ":3");
+//# sourceMappingURL=index.js.map
---
+++ source/index.ts
@@ -0,0 +1,3 @@
+import { Color3 } from "./types/RGB";
+
+console.log(Color3.fromRGB(0, 200, 0).toAnsi("hello world"), ":3")
