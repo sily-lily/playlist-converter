313ad285b5f409c0647d8b6591edcb4e11b24d30
lily <ilovesuno@proton.me>
2025-09-21 16:45:11 -0700

    Manual upload for now

--- source/modules/miscellaneous.ts
+++ source/modules/miscellaneous.ts
@@ -5,7 +5,9 @@ export function wait(
     setTimeout(callback, seconds * 1000);
 }
 
-export function removeDuplicates(input: string): string {
+export function removeDuplicates(
+  input: string
+): string {
   for (let index = 1; index <= input.length / 2; index++) {
     if (input.length % index === 0) {
       const subbed = input.slice(0, index);
