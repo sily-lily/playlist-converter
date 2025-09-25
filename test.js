3c67b3a7b3087131e1548b90bd176f179d7fe839
lily <ilovesuno@proton.me>
2025-09-25 14:07:59 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,6 @@
-console.log(5 % 5);
-console.log(11 % 5);
+const index = 1; // 0 based indexing
+const page = 2;
+// const formula = ((((index * page + (5 - index)) - (5 - index)) + page) - (4 - index));
+// const formula = index * page + (5 - index % 2 !== 0 ? (index + 1) : index);
+const formula = index + 4;
+console.log(formula);
